47d21280f4f9adb02e261058aa43d0be29a2bbeb
Jesse Shatsky <jesseshatsky@gmail.com>
2023-08-23 12:47:15 -0400

    initial commit

---
+++ components/FlowDiagram/config.ts
@@ -0,0 +1,11 @@
+import { type ComponentConfig } from "@superblocksteam/custom-components";
+
+export default {
+  // DO NOT CHANGE THE ID ONCE THE COMPONENT HAS BEEN REGISTERED!
+  id: "c44a8366-5b68-4abe-90c9-7b5e79f9c32f",
+  name: "FlowDiagram",
+  displayName: "Flow Diagram",
+  componentPath: "components/FlowDiagram/component.tsx",
+  properties: [],
+  events: [],
+} satisfies ComponentConfig;
